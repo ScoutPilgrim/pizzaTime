183926858bc3c767ab29bf46c710383ae911c6c2
Ian Christner <ichristn@ucsc.edu>
2019-04-12 13:04:45 -0700

    Added Pizza class using an explicit constructor

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
+//Pizza class setup and prototypes
+class Pizza{
+  constructor(sizeStr, toppingArr){
+    this.size = sizeStr;
+    this.toppings = toppingArr;
+    this.toppingsAmt = toppingArr.length;
+  }
+}
+
+
+//Front-end logic
 $(document).ready(function(){
   $('#additionalTopping').click(function(){
     $('form div.form-row:last').after($('form div.form-row:last').clone());
