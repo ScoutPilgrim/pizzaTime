b61211cca80661e622a70d4e9b67d39af9ca1a50
Ian Christner <ichristn@ucsc.edu>
2019-04-12 14:09:49 -0700

    Finally got dynamic elements properly building the toppingMap

--- js/scripts.js
+++ js/scripts.js
@@ -3,32 +3,42 @@ class Pizza{
   constructor(sizeStr, toppingMap){
     this.size = sizeStr;
     this.toppings = toppingMap;
-    this.toppingsAmt = toppingMap.length;
+    this.toppingsAmt = toppingMap.size;
   }
 }
 
 //global variables
 let toppingMap = new Map();
 let thisSize = 'X-Small';
+let idIter = 1;
 
 //Front-end logic
 $(document).ready(function(){
+  toppingMap.set(1, 'None'); //Setting the non dynamic Topping
   $('#pizzaForm').submit(function(event){
     event.preventDefault();
   });
   $('#pizzaSize').change(function(){
     thisSize = $('#pizzaSize').val();
-    console.log('Size chnaged to ' +thisSize);
+    console.log('Size changed to ' +thisSize);
   });
   $('#pizzaForm').on('change', '.thisTopping', function(event){
-    
+
   });
   $('#additionalTopping').click(function(){
+    idIter++;
     $('form div.form-row:last').after($('form div.form-row:last').clone());
     $('form div.form-row:last').find('.removeTopping').show();
+    $('form div.form-row:last').attr('id', idIter.toString());
+    toppingMap.set(idIter, 'None');
+    console.log(toppingMap.size);
   });
   $('#pizzaForm').on('click', '.removeBut', function(event){
-    console.log('removeBut is being clicked');
+    var thisId = parseInt($(this).parent().parent().attr('id'));
+    console.log(thisId);
+    console.log('Deleting topping at id: ' +thisId);
+    toppingMap.delete(thisId);
+    console.log(toppingMap.size);
     $(this).parent().parent().remove();
   });
 });
