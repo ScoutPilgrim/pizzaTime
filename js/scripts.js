564b33ac87d0ba297dd939c21c794695420d9525
Ian Christner <ichristn@ucsc.edu>
2019-04-12 12:45:49 -0700

    Had a rought itme getting event propagation up and running for dynamically generated form elements. Working now!

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
   $('#additionalTopping').click(function(){
     $('form div.form-row:last').after($('form div.form-row:last').clone());
+    $('form div.form-row:last').find('.removeTopping').show();
+  });
+  $('#pizzaForm').on('click', '.removeBut', function(event){
+    console.log('removeBut is being clicked');
+    $(this).parent().parent().remove();
   });
 });
